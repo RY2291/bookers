1746b48afda0548431b4dc5bdf17155cd22960fd
RY2291 <prgr2498@gmail.com>
2021-10-09 09:55:23 +0000

    [start]comment

--- db/migrate/20211009095133_create_book_comments.rb
+++ db/migrate/20211009095133_create_book_comments.rb
@@ -1,6 +1,9 @@
 class CreateBookComments < ActiveRecord::Migration[5.2]
   def change
     create_table :book_comments do |t|
+      t.string :comment
+      t.integer :user_id
+      t.integer :book_id
 
       t.timestamps
     end
